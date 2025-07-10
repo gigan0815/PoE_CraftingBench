d6bfb84a6da65c7df26974a8268788ec9c56eeb5
gigan0815 <amatecki92@gmail.com>
2025-07-10 21:54:43 +0200

    PoE window detection, Save mouse coordinates by keypress

--- main.cpp
+++ main.cpp
@@ -1,7 +1,72 @@
+#include <windows.h>
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+//
+// TODO:
+// -----------------------------------------------------------------------
+// Clean up code, check for functions
+// -----------------------------------------------------------------------
+// Add logic to use the stored coordinates
+// -----------------------------------------------------------------------
+// Saving coordinates by pressing 'a' feels unresponsive most of the time.
+// -----------------------------------------------------------------------
+//
+
+POINT StoreMouseCoordinates(string s)
+{
+    POINT cursorPos;
+
+    cout << "Press 'a' while hovering over the center of: " + s << endl;
+
+    while (true)
+    {
+        // Check if the 'A' key is pressed
+        if (GetAsyncKeyState('A') & 0x8000)
+        {
+            if (GetCursorPos(&cursorPos))
+            {
+                cout << "Cursor Position: (" << cursorPos.x << ", " << cursorPos.y << ")" << endl;
+                Sleep(100); // Sleep to avoid multiple captures on a single key press
+
+                return cursorPos;
+            }
+            else
+            {
+                cerr << "Error: Could not get cursor position." << endl;
+                exit(1);
+            }
+            break;
+        }
+        Sleep(100);
+    }
+}
 
 int main()
 {
-    cout << "Hi, first push to GitHub :-)";
+    // Set window title as constant
+    // This is the title of the Path of Exile window we want to bring to the foreground
+    // Ensure that the title matches exactly with the game's window title, else it cant be found
+    const char *windowTitle = "Path of Exile";
+
+    // Find the window by its title
+    HWND hwnd = FindWindowA(NULL, windowTitle);
+
+    // Check if the window was found
+    if (hwnd == NULL)
+    {
+        cerr << "Error: Could not find window." << endl;
+        exit(1);
+    }
+
+    // Bring the window to the foreground
+    SetForegroundWindow(hwnd);
+
+    POINT orbOfAlterationPos = StoreMouseCoordinates("Orb of Alteration");
+    POINT orbOfAugmentationPos = StoreMouseCoordinates("Orb of Augmentation");
+    POINT itemPos = StoreMouseCoordinates("Item");
+
+    return 0;
 }
